aa0153fe84b1f213602b92516efe5d6ff48160e6
Salomão Faleta <faletas41@gmail.com>
2023-09-10 22:08:57 -0300

    Bug, Colocando Conteúdo e Tamanho do Slide

--- SlideShow/script.js
+++ SlideShow/script.js
@@ -2,35 +2,43 @@
 const c = (el) => { return document.querySelector(el) };
 const cs = (el) => { return document.querySelectorAll(el) };
 
+//Pegando as imgs e adicionando a slider--width dinanmicamente com JS
 let totalSlides = document.querySelectorAll('.slider--item').length;
 c('.slider--width').style.width = `calc(100vw * ${totalSlides})`;
+
+//selecionando a altura dos controles e colocando na altura do slider
 c('.slider--controls').style.height = `${document.querySelector('.slider').clientHeight}px`;
 
 //variável de ambiente
 
 let currentSlide = 0;
 
-function goPrev(){
+//Funções para mudar de imagem
+
+function goPrev() {
     currentSlide--;
-    if(currentSlide < 0){
-        currentSlide = totalSlides - 1
+    if (currentSlide < 0) {
+        currentSlide = totalSlides - 1;
     }
+
     updateMargin();
 }
 
-function Next(){
+function Next() {
     currentSlide++;
-    if(currentSlide > (totalSlides -1)){
+    if(currentSlide > (totalSlides - 1)){
         currentSlide = 0;
     }
+
     updateMargin();
 }
 
-function updateMargin(){
-    let newMargin = (currentSlide * document.body.clientWidth);
+function updateMargin() {
+    let sliderItemWidth = c('.slider--item').clientWidth;
+
+    let newMargin = currentSlide * sliderItemWidth;
 
     c('.slider--width').style.marginLeft = `-${newMargin}px`;
-
 }
 
-setInterval(Next, 2000)
+setInterval(Next, 5000);
